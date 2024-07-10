b0ce21129a270682e4ed91874d7575564438eb80
Abdul Rehman <145242032+Digitalrehman@users.noreply.github.com>
2024-07-10 19:41:08 +0500

    Add files via upload

---
+++ src/assets/config.js
@@ -0,0 +1,24 @@
+let socialName = [
+  {
+    github: "digitalrehman",
+  },
+  {
+    linkedin: "rehmanarainoffical",
+  },
+  {
+    twitter: "rehman_digital",
+  },
+  {
+    instagram: "rehmanarainoffical",
+  },
+  {
+    rehmanofficial: "rehmanofficial",
+  },
+  {
+    tailwind: "tailwindcss",
+  },
+  {
+    discord: "abdulrehman0726",
+  },
+];
+export { socialName };
