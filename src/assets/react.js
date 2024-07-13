83458ca40c5b7e0d76135c68a22670ca7570ffee
Abdul Rehman <145242032+Digitalrehman@users.noreply.github.com>
2024-07-13 13:26:04 +0500

    Update react.js

--- src/assets/react.js
+++ src/assets/react.js
@@ -3,8 +3,8 @@ let reactCard = [
     id: 20,
     title: "E-Commerce",
     description:
-      "A comprehensive e-commerce platform built using React.js, Redux, and Bootstrap. This platform allows users to browse products, add items to their cart, and proceed to checkout. It features a clean and responsive design, ensuring a seamless shopping experience on all devices.",
-    techStack: ["React.js", "Redux", "Bootstrap"],
+      "A comprehensive e-commerce platform built using React, Redux, and Bootstrap. This platform allows users to browse products, add items to their cart, and proceed to checkout. It features a clean and responsive design, ensuring a seamless shopping experience on all devices.",
+    techStack: ["React", "Redux", "Bootstrap"],
     imageUrl:
       "https://res.cloudinary.com/daarlhxp1/image/upload/v1720355770/screencapture-ecommerce-2434-netlify-app-2024-07-07-17_30_46_wholxw.jpg",
     github: "https://github.com/Digitalrehman/E_Commerce",
@@ -20,15 +20,15 @@ let reactCard = [
     challenges:
       "Implementing state management with Redux and ensuring the application is responsive across various devices.",
     learnings:
-      "Gained a deeper understanding of React.js and Redux for state management, as well as Bootstrap for responsive design.",
+      "Gained a deeper understanding of React and Redux for state management, as well as Bootstrap for responsive design.",
     appFor: "Frontend Developer",
   },
   {
     id: 21,
     title: "Counter App",
     description:
-      "A simple yet effective counter application built using React.js and CSS. This app allows users to increment, decrement, and reset the counter value. It features a clean and minimalistic design, providing a smooth user experience.",
-    techStack: ["React.js", "CSS"],
+      "A simple yet effective counter application built using React and CSS. This app allows users to increment, decrement, and reset the counter value. It features a clean and minimalistic design, providing a smooth user experience.",
+    techStack: ["React", "CSS"],
     imageUrl:
       "https://res.cloudinary.com/daarlhxp1/image/upload/v1720355759/screencapture-counter-app12-netlify-app-2024-07-07-17_31_01_e4dz2n.jpg",
     github: "https://github.com/Digitalrehman/Counter",
@@ -51,8 +51,8 @@ let reactCard = [
     id: 22,
     title: "Landing Page",
     description:
-      "A captivating landing page built using React.js and CSS. This landing page is designed to attract and engage visitors with its modern design and smooth animations. It is fully responsive, ensuring it looks great on both desktop and mobile devices.",
-    techStack: ["React.js", "CSS"],
+      "A captivating landing page built using React and CSS. This landing page is designed to attract and engage visitors with its modern design and smooth animations. It is fully responsive, ensuring it looks great on both desktop and mobile devices.",
+    techStack: ["React", "CSS"],
     imageUrl:
       "https://res.cloudinary.com/daarlhxp1/image/upload/v1720355757/screencapture-modren-landing-netlify-app-2024-07-07-17_31_39_xvcp52.jpg",
     github: "https://github.com/Digitalrehman/React-Landing-Page",
@@ -68,15 +68,15 @@ let reactCard = [
     challenges:
       "Creating smooth animations and ensuring the design is fully responsive across all devices.",
     learnings:
-      "Enhanced my skills in React.js for component-based development and CSS for creating modern designs and animations.",
+      "Enhanced my skills in React for component-based development and CSS for creating modern designs and animations.",
     appFor: "Frontend Developer",
   },
   {
     id: 23,
     title: "ChatGPT Clone",
     description:
-      "A functional ChatGPT clone built using React.js, Core JS, and CSS. This application mimics the basic functionality of ChatGPT, allowing users to input text and receive AI-generated responses. It features a clean and user-friendly interface.",
-    techStack: ["React.js", "Core JS", "CSS"],
+      "A functional ChatGPT clone built using React, JavaScript, and CSS. This application mimics the basic functionality of ChatGPT, allowing users to input text and receive AI-generated responses. It features a clean and user-friendly interface.",
+    techStack: ["React", "JavaScript", "CSS"],
     imageUrl:
       "https://res.cloudinary.com/daarlhxp1/image/upload/v1720355756/screencapture-chatgpt-clo-netlify-app-2024-07-07-17_32_20_fdtfy3.jpg",
     github: "https://github.com/Digitalrehman/ChatGPY-Clone",
@@ -92,15 +92,15 @@ let reactCard = [
     challenges:
       "Implementing real-time AI responses and managing state effectively.",
     learnings:
-      "Improved my understanding of React.js for building interactive UIs, Core JS for handling complex logic, and CSS for styling.",
+      "Improved my understanding of React for building interactive UIs, JavaScript for handling complex logic, and CSS for styling.",
     appFor: "Frontend Developer",
   },
   {
     id: 24,
     title: "Gemini Clone",
     description:
-      "A Gemini clone built using React.js, Core JS, and CSS. This project replicates the basic functionality of the Gemini platform, providing a seamless user experience with features such as user authentication, portfolio management, and real-time market data.",
-    techStack: ["React.js", "Core JS", "CSS"],
+      "A Gemini clone built using React, JavaScript, and CSS. This project replicates the basic functionality of the Gemini platform, providing a seamless user experience with features such as user authentication, portfolio management, and real-time market data.",
+    techStack: ["React", "JavaScript", "CSS"],
     imageUrl:
       "https://res.cloudinary.com/daarlhxp1/image/upload/v1720355757/screencapture-gemini-clo-netlify-app-2024-07-07-17_32_55_ua5wxu.jpg",
     github: "private files",
@@ -116,15 +116,15 @@ let reactCard = [
     challenges:
       "Ensuring real-time updates for market data and managing user authentication securely.",
     learnings:
-      "Enhanced my skills in handling real-time data with Core JS, managing state and interactions with React.js, and applying CSS for a polished UI.",
+      "Enhanced my skills in handling real-time data with JavaScript, managing state and interactions with React, and applying CSS for a polished UI.",
     appFor: "Frontend Developer",
   },
   {
     id: 25,
     title: "Quotes Generator",
     description:
-      "A dynamic Quotes Generator built using React.js, Core JS, and CSS. This application allows users to generate random quotes with a single click. It features a clean and minimalistic design, ensuring a delightful user experience.",
-    techStack: ["React.js", "Core JS", "CSS"],
+      "A dynamic Quotes Generator built using React, JavaScript, and CSS. This application allows users to generate random quotes with a single click. It features a clean and minimalistic design, ensuring a delightful user experience.",
+    techStack: ["React", "JavaScript", "CSS"],
     imageUrl:
       "https://res.cloudinary.com/daarlhxp1/image/upload/v1720355757/screencapture-bright-cajeta-bde670-netlify-app-2024-07-07-17_32_34_yp4sfo.jpg",
     github: "https://github.com/Digitalrehman/Quotes-Generator",
@@ -140,15 +140,15 @@ let reactCard = [
     challenges:
       "Fetching and displaying quotes efficiently, and ensuring the design is appealing and responsive.",
     learnings:
-      "Improved my skills in working with APIs to fetch data, managing state in React.js, and applying CSS for a clean and responsive UI.",
+      "Improved my skills in working with APIs to fetch data, managing state in React, and applying CSS for a clean and responsive UI.",
     appFor: "Frontend Developer",
   },
   {
     id: 26,
     title: "Crypto Marketplace",
     description:
-      "A robust Crypto Marketplace built using React.js, Tailwind CSS, and Redux. This platform allows users to buy, sell, and trade various cryptocurrencies. It features a sleek and modern design, real-time market data, and secure transaction handling.",
-    techStack: ["React.js", "Tailwind CSS", "Redux"],
+      "A robust Crypto Marketplace built using React, Tailwind, and Redux. This platform allows users to buy, sell, and trade various cryptocurrencies. It features a sleek and modern design, real-time market data, and secure transaction handling.",
+    techStack: ["React", "Tailwind", "Redux"],
     imageUrl:
       "https://res.cloudinary.com/daarlhxp1/image/upload/v1720355758/screencapture-biggestcryptomarketplace-netlify-app-2024-07-07-17_33_29_efnrki.jpg",
     github: "private files",
@@ -164,15 +164,15 @@ let reactCard = [
     challenges:
       "Ensuring secure transactions and managing state effectively with Redux.",
     learnings:
-      "Gained a deeper understanding of state management with Redux, responsive design with Tailwind CSS, and implementing secure transaction handling.",
+      "Gained a deeper understanding of state management with Redux, responsive design with Tailwind, and implementing secure transaction handling.",
     appFor: "Frontend Developer",
   },
   {
     id: 27,
     title: "Spotify Clone",
     description:
-      "A feature-rich Spotify Clone built using React, Tailwind CSS, and Redux. This application replicates the core functionalities of Spotify, allowing users to browse, play, and manage their music playlists. It offers a sleek and responsive design for an optimal user experience on any device.",
-    techStack: ["React", "Tailwind CSS", "Redux"],
+      "A feature-rich Spotify Clone built using React, Tailwind, and Redux. This application replicates the core functionalities of Spotify, allowing users to browse, play, and manage their music playlists. It offers a sleek and responsive design for an optimal user experience on any device.",
+    techStack: ["React", "Tailwind", "Redux"],
     imageUrl:
       "https://res.cloudinary.com/daarlhxp1/image/upload/v1720355757/screencapture-spotify-clone167-netlify-app-2024-07-07-17_33_05_vfw4uv.jpg",
     github: "https://github.com/Digitalrehman/spotify-clone",
@@ -188,15 +188,15 @@ let reactCard = [
     challenges:
       "Managing complex state for music playback and user interactions, and ensuring a seamless responsive design.",
     learnings:
-      "Enhanced my skills in Redux for state management, Tailwind CSS for responsive and modern designs, and React for building interactive user interfaces.",
+      "Enhanced my skills in Redux for state management, Tailwind for responsive and modern designs, and React for building interactive user interfaces.",
     appFor: "Frontend Developer",
   },
   {
     id: 28,
     title: "Business Management",
     description:
-      "A comprehensive Business Management System built using React, Core JS, and CSS. This system helps businesses manage their operations, including employee management, inventory tracking, and sales reporting. It features a user-friendly interface and a responsive design.",
-    techStack: ["React", "Core JS", "CSS"],
+      "A comprehensive Business Management System built using React, JavaScript, and CSS. This system helps businesses manage their operations, including employee management, inventory tracking, and sales reporting. It features a user-friendly interface and a responsive design.",
+    techStack: ["React", "JavaScript", "CSS"],
     imageUrl:
       "https://res.cloudinary.com/daarlhxp1/image/upload/v1720412303/bussiness_cgdm4f.png",
     link: "https://business-mange.netlify.app/",
@@ -213,7 +213,7 @@ let reactCard = [
     challenges:
       "Implementing efficient state management for complex business operations and ensuring data accuracy.",
     learnings:
-      "Improved my understanding of handling complex state with Core JS, building interactive UIs with React, and creating responsive designs with CSS.",
+      "Improved my understanding of handling complex state with JavaScript, building interactive UIs with React, and creating responsive designs with CSS.",
     appFor: "Frontend Developer",
   },
 ];
