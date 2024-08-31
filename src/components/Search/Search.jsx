7903eef6ddda5c67d2139034c366a67d5a24ad9a
Abdul Rehman <145242032+digitalrehman@users.noreply.github.com>
2024-08-31 11:17:40 +0500

    Update Search.jsx

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -62,19 +62,19 @@ const Search = () => {
 
   return (
     <form className="max-w-md mx-auto" onSubmit={searchHandler}>
-    <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
+    <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only">
       Search
     </label>
-    <div className="relative flex items-center">
-      <div className="absolute inset-y-0 start-0 flex items-ce  nter ps-3 pointer-events-none">
-        <LuSearchCode className="w-6 h-6 text-gray-500 dark:text-gray-400" />
+    <div className="relative">
+      <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+        <LuSearchCode className="w-6 h-6 text-gray-500" />
       </div>
       <motion.input
         type="search"
         onChange={handleInputChange}
         value={search}
         id="default-search"
-        className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
         placeholder="Search Mockups, Logos..."
         initial={{ opacity: 0, y: -20 }} // Initial animation state
         animate={{ opacity: 1, y: 0 }}
@@ -85,7 +85,7 @@ const Search = () => {
       />
       <motion.button
         type="submit"
-        className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 0.95 }}
         style={{ opacity: inView ? 1 : 0 }} // Fade in animation when in view
